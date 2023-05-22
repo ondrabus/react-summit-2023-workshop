7b1ac3b23fa1b44aaeb8a7b916b02a248066a44b
Ondrej Polesny <ondrejp@kontent.ai>
2023-05-22 06:14:17 +0200

    Resets project to workshop start

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,7 @@ import { defineConfig } from 'astro/config';
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
-import react from "@astrojs/react";
-
-// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: netlify(),
-  integrations: [react()]
 });
--- src/lib/kontent.ts
+++
@@ -1,5 +0,0 @@
-import { createDeliveryClient } from "@kontent-ai/delivery-sdk";
-
-export const deliveryClient = createDeliveryClient({
-    environmentId: import.meta.env.KONTENT_PROJECT_ID
-})
